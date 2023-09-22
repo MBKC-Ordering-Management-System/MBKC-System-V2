d24bcfa3feab0d17615f5c2e6b381e450a4d8bce
TQToan <toantq.12c3.bc1819@gmail.com>
2023-09-22 14:02:12 +0700

    build(update product table): updated product table

--- MBKC_System/MBKC.DAL/DBContext/MBKCDbContext.cs
+++ MBKC_System/MBKC.DAL/DBContext/MBKCDbContext.cs
@@ -1,4 +1,5 @@
-﻿using MBKC.DAL.Models;
+﻿using MBKC.DAL.DataSeedings;
+using MBKC.DAL.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
@@ -246,6 +247,7 @@ namespace MBKC.DAL.DBContext
                 product.Property(prop => prop.Image).IsUnicode(true).HasMaxLength(int.MaxValue).IsRequired(true);
                 product.Property(prop => prop.HistoricalPrice).HasColumnType("decimal(9,2)").IsRequired(true);
                 product.Property(prop => prop.Status).IsRequired(true);
+                product.Property(prop => prop.DisplayOrder).IsRequired(true);
             });
 
             modelBuilder.Entity<Product>()
@@ -400,6 +402,10 @@ namespace MBKC.DAL.DBContext
            .WithOne()
            .HasForeignKey<StoreMoneyExchange>(storeMoneyExchange => storeMoneyExchange.ExchangeId);
             #endregion
+
+
+            modelBuilder.RoleData();
+
         }
     }
 }
