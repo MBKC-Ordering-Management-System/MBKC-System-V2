b7bc6ec6ae89e650df78562998926f75b8a85b70
bach951 <lexuanbach952001@gmail.com>
2023-10-03 13:45:11 +0700

    feat(Create Partner): Processing

---
+++ MBKC_System/MBKC.Service/DTOs/Partners/PostPartnerRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBKC.Service.DTOs.Partners
+{
+    public class PostPartnerRequest
+    {
+        public string Name { get; set; }
+        public string Logo { get; set; }
+        public string WebUrl { get; set; }
+        public int Status { get; set; }
+    }
+}
