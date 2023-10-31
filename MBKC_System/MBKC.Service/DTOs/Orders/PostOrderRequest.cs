2bd14a81ab92dbd86c48105a2cac106cc1776487
TQToan <toantq.12c3.bc1819@gmail.com>
2023-10-31 16:27:03 +0700

    fix(change datatype commission of order): done chnage datatype

--- MBKC_System/MBKC.Service/DTOs/Orders/PostOrderRequest.cs
+++ MBKC_System/MBKC.Service/DTOs/Orders/PostOrderRequest.cs
@@ -15,7 +15,7 @@ namespace MBKC.Service.DTOs.Orders
         public decimal SubTotalPrice { get; set; }
         public decimal TotalDiscount { get; set; }
         public decimal FinalTotalPrice { get; set; }
-        public decimal Commission { get; set; }
+        public float Commission { get; set; }
         public decimal Tax { get; set; }
         public string Status { get; set; }
         public int PartnerId { get; set; }
